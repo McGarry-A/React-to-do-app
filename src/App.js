2760c01e5c8484d3e20c1c42efb8d8a1000a2799
Ahmed mcgarry <ahmedmcgarry@Ahmeds-Mac-mini.local>
2021-10-22 11:46:20 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [toDoList, setToDoList] = useState(["card1"]);
+  const [toDoItem, setToDoItem] = useState("");
+
+  const updateInputValue = event => setToDoItem(event.target.value);
+
+  const addCardHandler = () => {
+    let newArray = [...toDoList]
+    newArray.push(toDoItem)
+    setToDoList(newArray)
+  };
+
+  const deleteHandler = index => {
+    console.log("deleteHandler called")
+    let storedList = [...toDoList]
+    storedList.splice(index, 1)
+    setToDoList(storedList)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To Do List</h1>
+      <input value={ toDoItem } onChange={ updateInputValue }></input>
+      <button onClick={addCardHandler}>Add To Do Item</button>
+      {toDoList.map((el, index) => {
+        return <ToDoItem name={el} key={index} index={index} func={deleteHandler}/>;
+      })}
     </div>
   );
 }
+
+const ToDoItem = (props) => {
+  return (
+    <div>
+      <h3>{props.name}</h3>
+      <button index={props.index} onClick={()=>{props.func(props.index)}}>Delete</button>
+    </div>
+  );
+};
 
 export default App;
